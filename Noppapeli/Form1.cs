a5c703f7edc28d303958556907014d3b9f2eedb0
SakkyTT <teemu.turunen@sakky.fi>
2022-09-23 08:02:43 +0300

    Add project files.

---
+++ Noppapeli/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Noppapeli
+{
+    // Luo luokka "Noppa", jossa on tallessa nopan arvo 1-6
+    // nopalla on myös "Heitto"-metodi, joka arpoo sille arvon 1-6
+    // Anna nopalle myös constructor-metodi, joka heti alussa arpoo arvon
+
+    // Lähde tekemään Yatzi-peli
+
+    public partial class Form1 : Form
+    {
+        // property
+        Noppa noppa1 = new Noppa(20);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            noppa1.Heitto();
+
+            label1.Text = noppa1.Luku.ToString();
+            // lisää nopalle property "Koko", jolla määritellään
+            // montako silmälukua nopalla on
+            // ja käytetään sitä luvun arvonnassa
+            // nopan koko annetaan sitä generoidessa
+        }
+    }
+}
---
+++ Noppapeli/Noppa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noppapeli
+{
+    class Noppa
+    {
+        // property
+        public int Luku;
+        public int Koko;
+
+        // method
+        public Noppa(int koko) // constructor
+        {
+            Koko = koko;
+            Heitto();
+        }
+        public void Heitto()
+        {
+            Random rng = new Random();
+            Luku = rng.Next(1, Koko+1);
+        }
+    }
+}
