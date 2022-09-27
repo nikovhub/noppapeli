5aedb8a1e27d72562e9f0c1565f8fcaf123b4db7
nikovhub <nikowirta@hotmail.com>
2022-09-27 13:23:32 +0300

    isegnlt

--- Noppapeli/Form1.cs
+++ Noppapeli/Form1.cs
@@ -21,21 +21,105 @@ namespace Noppapeli
     public partial class Form1 : Form
     {
         // property
-        Noppa noppa1 = new Noppa(20);
+        //Noppa noppa1 = new Noppa(6);
+        List<Noppa> Nopat = new List<Noppa>();
         public Form1()
         {
             InitializeComponent();
+            // luodaan 5 noppaa
+            for (int i = 0; i < 5; i++)
+            {
+                PictureBox tempPB = new PictureBox();
+
+                this.Controls.Add(tempPB);
+
+
+                Noppa tempNoppa = new Noppa(6, tempPB);
+                Nopat.Add(tempNoppa);
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            noppa1.Heitto();
-
-            label1.Text = noppa1.Luku.ToString();
+            for (int i = 0; i < Nopat.Count; i++)
+            {
+                Nopat[i].Heitto();
+                editPictureBox(Nopat[i], i);
+            }
+           
+                //noppa1.Heitto();
+                //addPictureBox(noppa1, 1);
+                //label1.Text = noppa1.Luku.ToString();
+                //addPictureBox(noppa1, 1);
+            
             // lisää nopalle property "Koko", jolla määritellään
             // montako silmälukua nopalla on
             // ja käytetään sitä luvun arvonnassa
             // nopan koko annetaan sitä generoidessa
         }
+
+        private void addPictureBox(Noppa jokuNoppa, int Luku)
+        {
+            const int spacing = 60;
+
+
+
+
+            PictureBox tempPB = new PictureBox();
+            string key = jokuNoppa.GetPictureKey();
+            tempPB.Image = Noppa.GetPictureResourceX(key); 
+            tempPB.Location = new Point(13, 13);
+            tempPB.Size = new Size(100, 200);
+            tempPB.SizeMode = PictureBoxSizeMode.Zoom;
+
+            this.Controls.Add(tempPB);
+
+        }
+        private void editPictureBox(Noppa jokuNoppa, int Luku)
+        {
+            const int spacing = 60;
+
+            string key = jokuNoppa.GetPictureKey();
+            jokuNoppa.Boxi.Image = Noppa.GetPictureResourceX(key);
+            jokuNoppa.Boxi.Location = new Point(13 + Luku * spacing, 13);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int ones = 0;
+            for (var i = 0; i < Nopat.Count; i++)
+            {
+                if(i == 1)
+                {
+                    ones += 1;
+                }
+            }
+            txtYks.Text = ones.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int[] pairs = new int[6];
+            // { 0, 4, 0, 0, 10, 0 }
+            int[] pairValues = new int[6];
+            const int multiplier = 2;
+
+            for (int i = 0; i < pairs.Length; i++)
+            {
+                //LinQ kirjaston metodeja
+                pairs[i] = Nopat.Where(noppa => noppa.Luku == i + 1).Count();
+
+            }
+            for (int i = 0; i <pairs.Length; i++)
+            {
+                if (pairs[i] > 1) //lasketaan vain parit + yli
+                {
+                    pairValues[i] = (i + 1) * multiplier;
+                }
+            }
+            txtPari.Text = pairValues.Max().ToString();
+        }
+
+        
     }
 }
--- Noppapeli/Noppa.cs
+++ Noppapeli/Noppa.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Noppapeli
 {
@@ -11,18 +13,39 @@ namespace Noppapeli
         // property
         public int Luku;
         public int Koko;
+        public PictureBox Boxi;
+        //public PictureBox Boxi= new PictureBox();
 
         // method
-        public Noppa(int koko) // constructor
+        public Noppa(int koko, PictureBox boxi) // constructor
         {
+            Boxi = boxi;
             Koko = koko;
             Heitto();
-            Heitto();
+            Boxi.Size = new Size(40, 40);
+            Boxi.SizeMode = PictureBoxSizeMode.Zoom;
         }
         public void Heitto()
         {
             Random rng = new Random();
             Luku = rng.Next(1, Koko+1);
         }
+
+        public string GetPictureKey()
+        {
+            string returnValue = "_"; // kuvien nimet alkavat _ merkillä + nopan numeerinen luku. Tämä funktio kasaa halutun kuvan nimen
+                                      // yhdistämällä _ ja nopan luvun.
+            returnValue += Luku.ToString();
+
+
+            return returnValue;
+        }
+
+        public static Image GetPictureResourceX(string key)
+        {
+            return Noppapeli.noppakuvat.ResourceManager.GetObject(key) as Image;
+        }
+
+
     }
 }
